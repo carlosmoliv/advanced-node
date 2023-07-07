62f602338ac012f9f885a970983c44cde2ef6bd3
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-07-06 23:19:54 -0300

    refactor: move production code from FacebookApi test to their own files

---
+++ src/infra/apis/facebook.ts
@@ -0,0 +1,24 @@
+import { LoadFacebookUserApi } from '@/data/contracts/apis'
+import { HttpGetClient } from '../http'
+
+export class FacebookApi {
+  private readonly baseUrl = 'https://graph.facebook.com'
+
+  constructor(
+    private readonly httpClient: HttpGetClient,
+    private readonly clientId: string,
+    private readonly clientSecret: string
+  ) {}
+
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  async loadUser(params: LoadFacebookUserApi.Params): Promise<void> {
+    await this.httpClient.get({
+      url: `${this.baseUrl}/oauth/access_token`,
+      params: {
+        client_id: this.clientId,
+        client_secret: this.clientSecret,
+        grant_type: 'client_credentials',
+      },
+    })
+  }
+}
