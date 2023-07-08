502dd7a0a628d47a0f43f2d1a042e516c203d323
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-07-08 19:54:51 -0300

    feat: ensure Axios HttpClient rethrows if axios throws

--- tests/infra/http/axios-client.spec.ts
+++ tests/infra/http/axios-client.spec.ts
@@ -40,5 +40,13 @@ describe('AxiosHttpClient', () => {
 
       expect(result).toBe('any_data')
     })
+
+    it('should rethrow if get throws', async () => {
+      fakeAxios.get.mockRejectedValueOnce(new Error('http_error'))
+
+      const promise = sut.get({ url, params })
+
+      await expect(promise).rejects.toThrow(new Error('http_error'))
+    })
   })
 })
