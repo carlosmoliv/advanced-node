4cb3b252ffbd97837ebcacb6f87d223268f6cebc
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-07-16 15:34:38 -0300

    refactor: ensure ValidationComposite returns the first error found

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -4,11 +4,17 @@ interface Validator {
   validate: () => Error | undefined
 }
 
-class ValidationComposite {
+class ValidationComposite implements Validator {
   constructor(private readonly validators: Validator[]) {}
 
-  validate(): undefined {
-    return undefined
+  validate(): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate()
+
+      if (error !== undefined) {
+        return error
+      }
+    }
   }
 }
 
@@ -37,5 +43,15 @@ describe('ValidationComposite', () => {
     const error = sut.validate()
 
     expect(error).toBeUndefined()
+  })
+
+  it('should return the first error', () => {
+    validator1.validate.mockReturnValue(new Error('error_1'))
+    validator2.validate.mockReturnValue(new Error('error_2'))
+
+    // eslint-disable-next-line @typescript-eslint/no-confusing-void-expression
+    const error = sut.validate()
+
+    expect(error).toEqual(new Error('error_1'))
   })
 })
