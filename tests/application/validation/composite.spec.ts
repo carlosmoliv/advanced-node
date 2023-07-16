1434f0f367b585edf47d338aaf8b5216af29a90d
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-07-16 15:40:35 -0300

    refactor: move production code from composite test suite to their own files

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,22 +1,5 @@
 import { type MockProxy, mock } from 'jest-mock-extended'
-
-interface Validator {
-  validate: () => Error | undefined
-}
-
-class ValidationComposite implements Validator {
-  constructor(private readonly validators: Validator[]) {}
-
-  validate(): Error | undefined {
-    for (const validator of this.validators) {
-      const error = validator.validate()
-
-      if (error !== undefined) {
-        return error
-      }
-    }
-  }
-}
+import { type Validator, ValidationComposite } from '@/application/validation'
 
 describe('ValidationComposite', () => {
   let sut: ValidationComposite
