afcbfe03b6ac61169ddad62df9539def4af37127
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-07-18 21:46:45 -0300

    featchore: add jest config to integration tests

--- jest.config.js
+++ jest.config.js
@@ -5,16 +5,21 @@
 
 module.exports = {
   // An array of glob patterns indicating a set of files for which coverage information should be collected
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts', '!<rootDir>/src/main/**', '!<rootDir>/src/**/index.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/main/**',
+    '!<rootDir>/src/**/index.ts'
+  ],
   coverageDirectory: 'coverage',
   coverageProvider: 'babel',
   moduleNameMapper: {
     '@/tests/(.+)': '<rootDir>/tests/$1',
-    '@/(.+)': '<rootDir>/src/$1',
+    '@/(.+)': '<rootDir>/src/$1'
   },
   roots: ['<rootDir>/src', '<rootDir>/tests'],
+  testMatch: ['**/*.spec.ts'],
   transform: {
-    '\\.ts$': 'ts-jest',
+    '\\.ts$': 'ts-jest'
   },
-  clearMocks: true,
+  clearMocks: true
 }
