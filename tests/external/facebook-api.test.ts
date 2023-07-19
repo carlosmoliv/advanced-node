1821de40cb084db7699e8608759a77b90c69e920
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-07-18 23:18:19 -0300

    feat: ensure Facebook Api returns a facebook user if token is valid

--- tests/external/facebook-api.test.ts
+++ tests/external/facebook-api.test.ts
@@ -1,5 +1,37 @@
-describe('Test', () => {
-  it('Test', () => {
-    expect(1).toBe(1)
+import { FacebookApi } from '@/infra/apis'
+import { AxiosHttpClient } from '@/infra/http'
+import { env } from '@/main/config/env'
+
+describe('FacebookApi Integration Tests', () => {
+  let axiosClient: AxiosHttpClient
+  let sut: FacebookApi
+
+  beforeEach(async () => {
+    axiosClient = new AxiosHttpClient()
+    sut = new FacebookApi(
+      axiosClient,
+      env.facebookApi.clientId,
+      env.facebookApi.clientSecret
+    )
+  })
+
+  it('should return a Facebook User if token is valid', async () => {
+    const fbUser = await sut.loadUser({
+      token: env.facebookApi.user.token
+    })
+
+    expect(fbUser).toEqual({
+      facebookId: env.facebookApi.user.facebookUserId,
+      name: env.facebookApi.user.name,
+      email: env.facebookApi.user.email
+    })
+  })
+
+  it('should return undefined if token is valid', async () => {
+    const fbUser = await sut.loadUser({
+      token: 'invalid_token'
+    })
+
+    expect(fbUser).toBeUndefined()
   })
 })
