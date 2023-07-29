e7fec7a58b1af559270e9b7671b982bf2a8a8825
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-07-28 21:44:06 -0300

    feat:  ensure AuthenticationMiddleware returns 403 if auhtorization is falsy

---
+++ tests/application/middlewares/authentication.spec.ts
@@ -0,0 +1,45 @@
+import { forbidden, type HttpResponse } from '@/application/helpers'
+import { ForbiddenError } from '@/application/errors'
+
+type HttpRequest = { authorization: string }
+
+class AuthenticationMiddleware {
+  async handle(httpRequest: HttpRequest): Promise<HttpResponse<Error>> {
+    return forbidden()
+  }
+}
+
+describe('AuthenticationMiddleware', () => {
+  it('should return 403 if auhtorization is empty', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: '' })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
+
+  it('should return 403 if auhtorization is null', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: null as any })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
+
+  it('should return 403 if auhtorization is undefined', async () => {
+    const sut = new AuthenticationMiddleware()
+
+    const httpResponse = await sut.handle({ authorization: undefined as any })
+
+    expect(httpResponse).toEqual({
+      statusCode: 403,
+      data: new ForbiddenError()
+    })
+  })
+})
