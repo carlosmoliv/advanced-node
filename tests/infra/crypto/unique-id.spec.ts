0a2e6ac7374909c2c95ec4c3804e5e5a553f0e2b
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-08-03 19:44:09 -0300

    feat: add UniqueId and its tests

---
+++ tests/infra/crypto/unique-id.spec.ts
@@ -0,0 +1,19 @@
+import { UniqueId } from '@/infra/crypto'
+
+describe('UniqueId', () => {
+  it('should vall uuid.v4', () => {
+    const sut = new UniqueId(new Date(2021, 9, 3, 10, 10, 10))
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_20211003101010')
+  })
+
+  it('should vall uuid.v4', () => {
+    const sut = new UniqueId(new Date(1993, 5, 21, 2, 8, 30))
+
+    const uuid = sut.uuid({ key: 'any_key' })
+
+    expect(uuid).toBe('any_key_19930621020830')
+  })
+})
