9433f7d26c50e8f3756c786b63c9ecea36b6c6cb
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-08-03 22:42:45 -0300

    feat: ensure AwsS3FileStorage calls putObject with correct input

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -1,33 +1,57 @@
-import { config } from 'aws-sdk'
+import { type UploadFile } from '@/domain/contracts/gateways'
+
+import { S3, config } from 'aws-sdk'
 
 jest.mock('aws-sdk')
 
 class AwsS3FileStorage {
   constructor (
-    private readonly accessKey: string,
-    private readonly secret: string
+    accessKey: string,
+    secret: string,
+    private readonly bucket: string
   ) {
     config.update({
       credentials: {
-        accessKeyId: this.accessKey,
-        secretAccessKey: this.secret
+        accessKeyId: accessKey,
+        secretAccessKey: secret
       }
     })
+  }
+
+  async upload ({ file, key }: UploadFile.Input): Promise<void> {
+    const s3 = new S3()
+    await s3.putObject({
+      Bucket: this.bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
+    }).promise()
   }
 }
 
 describe('AwsS3FileStorage', () => {
-  let sut: AwsS3FileStorage
   let accessKey: string
   let secret: string
+  let bucket: string
+  let key: string
+  let file: Buffer
+  let sut: AwsS3FileStorage
+  let putObjectPromiseSpy: jest.Mock
+  let putObjectSpy: jest.Mock
 
   beforeAll(() => {
     accessKey = 'any_access_key'
     secret = 'any_secret'
+    bucket = 'any_bucket'
+    key = 'any_key'
+    file = Buffer.from('any_buffer')
+    putObjectPromiseSpy = jest.fn()
+    putObjectSpy = jest.fn().mockImplementation(() => ({ promise: putObjectPromiseSpy }))
+    jest.mocked(S3).mockImplementation(jest.fn().mockImplementation(() => ({ putObject: putObjectSpy })))
   })
 
   beforeEach(() => {
-    sut = new AwsS3FileStorage(accessKey, secret)
+    sut = new AwsS3FileStorage(accessKey, secret, bucket)
   })
 
   it('should call upload with correct input', async () => {
@@ -39,5 +63,18 @@ describe('AwsS3FileStorage', () => {
       }
     })
     expect(config.update).toHaveBeenCalledTimes(1)
+  })
+
+  it('should call putObject with correct input', async () => {
+    await sut.upload({ key, file })
+
+    expect(putObjectSpy).toHaveBeenCalledWith({
+      Bucket: bucket,
+      Key: key,
+      Body: file,
+      ACL: 'public-read'
+    })
+    expect(putObjectSpy).toHaveBeenCalledTimes(1)
+    expect(putObjectPromiseSpy).toHaveBeenCalledTimes(1)
   })
 })
