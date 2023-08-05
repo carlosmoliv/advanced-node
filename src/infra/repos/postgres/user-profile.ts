7e586812fef8c102d559aed44460f0f70aaba877
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-08-05 13:53:38 -0300

    feat: ensure PgUserProfileRepository loads profile

--- src/infra/repos/postgres/user-profile.ts
+++ src/infra/repos/postgres/user-profile.ts
@@ -1,11 +1,17 @@
 import { PgUser } from '@/infra/repos/postgres/entities'
-import { type SaveUserPicture } from '@/domain/contracts/repos'
+import { type LoadUserProfile, type SaveUserPicture } from '@/domain/contracts/repos'
 
 import { getRepository } from 'typeorm'
 
 export class PgUserProfileRepository implements SaveUserPicture {
   async savePicture ({ id, pictureUrl, initials }: SaveUserPicture.Input): Promise<void> {
     const pgUserRepo = getRepository(PgUser)
-    await pgUserRepo.update({ id: Number(id) }, { pictureUrl, initials })
+    await pgUserRepo.update({ id: parseInt(id) }, { pictureUrl, initials })
+  }
+
+  async load ({ id, pictureUrl, initials }: SaveUserPicture.Input): Promise<LoadUserProfile.Output> {
+    const pgUserRepo = getRepository(PgUser)
+    const pgUser = await pgUserRepo.findOne({ id: parseInt(id) })
+    if (pgUser !== undefined) return { name: pgUser.name }
   }
 }
--- tests/infra/repos/postgres/user-profile.spec.ts
+++ tests/infra/repos/postgres/user-profile.spec.ts
@@ -35,4 +35,14 @@ describe('PgUseraProfileRepository', () => {
       expect(pgUser).toMatchObject({ id, initials: null, pictureUrl: 'any_url' })
     })
   })
+
+  describe('loadUserProfile()', () => {
+    it('should load user profile', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any_email', name: 'any_name' })
+
+      const userProfile = await sut.load({ id: id.toString() })
+
+      expect(userProfile?.name).toBe('any_name')
+    })
+  })
 })
