a0bf80b3bc7cd26a2318bc1fc70aeae65084af69
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-08-05 13:28:17 -0300

    feat:  ensure PguserProfileRepository updates  use profile

---
+++ tests/infra/repos/postgres/user-profile.spec.ts
@@ -0,0 +1,38 @@
+import { PgUser } from '@/infra/repos/postgres/entities'
+import { PgUserProfileRepository } from '@/infra/repos/postgres/user-profile'
+import { makeFakeDb } from '@/tests/infra/repos/postgres/mocks'
+
+import { type IBackup } from 'pg-mem'
+import { type Repository, getConnection, getRepository } from 'typeorm'
+
+describe('PgUseraProfileRepository', () => {
+  let sut: PgUserProfileRepository
+  let pgUserRepo: Repository<PgUser>
+  let backup: IBackup
+
+  beforeAll(async () => {
+    const db = await makeFakeDb([PgUser])
+    backup = db.backup()
+    pgUserRepo = getRepository(PgUser)
+  })
+
+  afterAll(async () => {
+    await getConnection().close()
+  })
+
+  beforeEach(() => {
+    backup.restore()
+    sut = new PgUserProfileRepository()
+  })
+
+  describe('savePicture()', () => {
+    it('should update user profile', async () => {
+      const { id } = await pgUserRepo.save({ email: 'any_email', initials: 'any_initials' })
+
+      await sut.savePicture({ id: id.toString(), pictureUrl: 'any_url' })
+      const pgUser = await pgUserRepo.findOne({ id })
+
+      expect(pgUser).toMatchObject({ id, initials: null, pictureUrl: 'any_url' })
+    })
+  })
+})
