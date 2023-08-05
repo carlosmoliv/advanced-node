e5fd4ab7a696dead2d70f3e1f5dbe804d463acc6
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-08-05 15:33:42 -0300

    feat: isolate SUT creation on SavePictureController test suite

--- tests/application/controllers/save-picture-controller.spec.ts
+++ tests/application/controllers/save-picture-controller.spec.ts
@@ -5,16 +5,38 @@ type HttpRequest = { file: any }
 type Model = Error
 
 class SavePictureController {
-  async handle ({ file }: HttpRequest): Promise<HttpResponse<Model>> {
+  async handle({ file }: HttpRequest): Promise<HttpResponse<Model>> {
     return badRequest(new RequiredFieldError('file'))
   }
 }
 
 describe('SavePictureController', () => {
-  it('should return 400 if file is not provided', async () => {
-    const sut = new SavePictureController()
+  let sut: SavePictureController
 
+  beforeEach(() => {
+    sut = new SavePictureController()
+  })
+
+  it('should return 400 if file is not provided', async () => {
     const httpResponse = await sut.handle({ file: undefined })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('should return 400 if file is not provided', async () => {
+    const httpResponse = await sut.handle({ file: null })
+
+    expect(httpResponse).toEqual({
+      statusCode: 400,
+      data: new RequiredFieldError('file')
+    })
+  })
+
+  it('should return 400 if file is not provided', async () => {
+    const httpResponse = await sut.handle({ file: null })
 
     expect(httpResponse).toEqual({
       statusCode: 400,
