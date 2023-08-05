921bba433f839f03f828a118d0be97ae7afe84e9
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-08-05 15:16:27 -0300

    feat: ensure DeletePictureController extends Controller

--- tests/application/controllers/delete-profile-picture.controller.spec.ts
+++ tests/application/controllers/delete-profile-picture.controller.spec.ts
@@ -1,12 +1,15 @@
 import { type ChangeProfilePicture } from '@/domain/use-cases'
 import { notContent, type HttpResponse } from '@/application/helpers'
+import { Controller } from '@/application/controllers'
 
 type HttpRequest = { userId: string }
 
-class DeletePictureController {
-  constructor(private readonly changeProfilePicture: ChangeProfilePicture) { }
+class DeletePictureController extends Controller {
+  constructor (private readonly changeProfilePicture: ChangeProfilePicture) {
+    super()
+  }
 
-  async handle({ userId }: HttpRequest): Promise<HttpResponse> {
+  async perform ({ userId }: HttpRequest): Promise<HttpResponse> {
     await this.changeProfilePicture({ id: userId })
     return notContent()
   }
@@ -19,6 +22,10 @@ describe('DeletePictureController', () => {
   beforeEach(() => {
     changeProfilePicture = jest.fn()
     sut = new DeletePictureController(changeProfilePicture)
+  })
+
+  it('should extends Controller', async () => {
+    expect(sut).toBeInstanceOf(Controller)
   })
 
   it('should ChangeProfilePicture with correct input', async () => {
