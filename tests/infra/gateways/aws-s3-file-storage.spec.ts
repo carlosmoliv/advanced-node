51dbf50e43a7e29c50a417145d826a1e6fb5bb9c
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-08-04 22:07:17 -0300

    test: ensure AwsS3FileStorager returns imageUrl

--- tests/infra/gateways/aws-s3-file-storage.spec.ts
+++ tests/infra/gateways/aws-s3-file-storage.spec.ts
@@ -4,7 +4,7 @@ import { S3, config } from 'aws-sdk'
 
 jest.mock('aws-sdk')
 
-class AwsS3FileStorage {
+class AwsS3FileStorage implements UploadFile {
   constructor (
     accessKey: string,
     secret: string,
@@ -18,7 +18,7 @@ class AwsS3FileStorage {
     })
   }
 
-  async upload ({ file, key }: UploadFile.Input): Promise<void> {
+  async upload ({ file, key }: UploadFile.Input): Promise<UploadFile.Output> {
     const s3 = new S3()
     await s3.putObject({
       Bucket: this.bucket,
@@ -26,6 +26,8 @@ class AwsS3FileStorage {
       Body: file,
       ACL: 'public-read'
     }).promise()
+
+    return `https://${this.bucket}.s3.amazonaws.com/${encodeURIComponent(key)}`
   }
 }
 
@@ -76,5 +78,17 @@ describe('AwsS3FileStorage', () => {
     })
     expect(putObjectSpy).toHaveBeenCalledTimes(1)
     expect(putObjectPromiseSpy).toHaveBeenCalledTimes(1)
+  })
+
+  it('should return imageUrl', async () => {
+    const imageUrl = await sut.upload({ key, file })
+
+    expect(imageUrl).toBe(`https://${bucket}.s3.amazonaws.com/${key}`)
+  })
+
+  it('should return imageUrl', async () => {
+    const imageUrl = await sut.upload({ key: 'any key', file })
+
+    expect(imageUrl).toBe(`https://${bucket}.s3.amazonaws.com/any%20key`)
   })
 })
