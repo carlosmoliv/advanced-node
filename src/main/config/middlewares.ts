a8d427b1231649f883b84bf01e1b3246b37c2857
Carlos Oliveira <carlosmoliveira93@gmail.com>
2023-08-23 20:25:02 -0300

    fix: add explicit config on cors

--- src/main/config/middlewares.ts
+++ src/main/config/middlewares.ts
@@ -2,7 +2,11 @@ import { type Express, json } from 'express'
 import cors from 'cors'
 
 export const setupMiddlewares = (app: Express): void => {
-  app.use(cors())
+  app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }))
   app.use(json())
   app.use((req, res, next) => {
     res.type('json')
